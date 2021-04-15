65a2021afac643092ade62f6604306f107aee1f6
Johan Abrahamsson <71386129+Johan-Abrhamsson@users.noreply.github.com>
2021-04-15 10:02:51 +0200

    first commit

---
+++ Trainline/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Trainline
+{
+    class city
+    {
+        private int[] resources = new int[15];
+        // 0 = Steel
+
+        private string name = "";
+        private int Pop = 0;
+        private int level = 0;
+    }
+    class Track
+    {
+        private string name = "";
+        private int x = 0;
+        private int y = 0;
+        private List<int> order = new List<int>();
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> Namn = new List<string>();
+        }
+    }
+}
+
